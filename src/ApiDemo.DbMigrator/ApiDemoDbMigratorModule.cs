c050d7cc621217f550f5b9dd7190a253fa091f46
Sergiusz Kriwobokow <writesd@hotmail.com>
2023-02-16 03:00:29 +0100

    Add project files.

---
+++ src/ApiDemo.DbMigrator/ApiDemoDbMigratorModule.cs
@@ -0,0 +1,15 @@
+﻿using ApiDemo.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace ApiDemo.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(ApiDemoEntityFrameworkCoreModule),
+    typeof(ApiDemoApplicationContractsModule)
+    )]
+public class ApiDemoDbMigratorModule : AbpModule
+{
+
+}
